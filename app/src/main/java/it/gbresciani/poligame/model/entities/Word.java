42b50c2210e06cb1f738b822357f73e784b5d27d
Giacomo Bresciani <brescia123@gmail.com>
2015-02-09 18:20:51 +0100

    word entity first implementation

---
+++ app/src/main/java/it/gbresciani/poligame/model/entities/Word.java
@@ -0,0 +1,28 @@
+package it.gbresciani.poligame.model.entities;
+
+import com.orm.SugarRecord;
+
+public class Word extends SugarRecord<Word> {
+    private String lemma;
+    private String syllable1;
+    private String syllable2;
+    private String inverSyllable1;
+    private String inverSyllable2;
+
+    public Word() {
+    }
+
+    public Word(String lemma, String syllable1, String syllable2) {
+        this.lemma = lemma;
+        this.syllable1 = syllable1;
+        this.syllable2 = syllable2;
+    }
+
+    public Word(String lemma, String syllable1, String syllable2, String inverSyllable1, String inverSyllable2) {
+        this.lemma = lemma;
+        this.syllable1 = syllable1;
+        this.syllable2 = syllable2;
+        this.inverSyllable1 = inverSyllable1;
+        this.inverSyllable2 = inverSyllable2;
+    }
+}
