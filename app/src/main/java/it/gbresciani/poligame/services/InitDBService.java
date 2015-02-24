91a74a1c0475df090985dee46ddba2416a45d700
Giacomo Bresciani <brescia123@gmail.com>
2015-02-24 19:18:52 +0100

    first implementation

--- app/src/main/java/it/gbresciani/poligame/services/InitDBService.java
+++ app/src/main/java/it/gbresciani/poligame/services/InitDBService.java
@@ -2,6 +2,17 @@ package it.gbresciani.poligame.services;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.res.AssetManager;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.gbresciani.poligame.model.Word;
 
@@ -9,14 +20,40 @@ public class InitDBService extends IntentService {
 
     private final static String TAG = "InitDBService";
 
+    private Gson gson;
+
     public InitDBService() {
         super("InitDBService");
+        gson = new Gson();
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Word word = new Word("casa", "ca", "sa", "", "");
-        word.save();
+
+        AssetManager assetManager = getAssets();
+
+        try {
+            InputStream JSONInputStream = assetManager.open("words.json");
+            parseAndSaveWords(JSONInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    * Parses the input stream and returns a List of Word objects
+    */
+    private void parseAndSaveWords(InputStream in) throws IOException {
+
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            Word word = gson.fromJson(reader, Word.class);
+            word.save();
+        }
+        reader.endArray();
+        reader.close();
     }
 }
 
