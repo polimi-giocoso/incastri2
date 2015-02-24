df2036014e9e3b05e22978ca223058dd5f9b1b88
Giacomo Bresciani <brescia123@gmail.com>
2015-02-24 23:46:16 +0100

    posting loading events

--- app/src/main/java/it/gbresciani/poligame/services/InitDBService.java
+++ app/src/main/java/it/gbresciani/poligame/services/InitDBService.java
@@ -3,17 +3,19 @@ package it.gbresciani.poligame.services;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+import com.squareup.otto.Bus;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
+import it.gbresciani.poligame.events.LoadingEvent;
+import it.gbresciani.poligame.helper.BusProvider;
 import it.gbresciani.poligame.model.Word;
 
 public class InitDBService extends IntentService {
@@ -21,6 +23,8 @@ public class InitDBService extends IntentService {
     private final static String TAG = "InitDBService";
 
     private Gson gson;
+    private Bus BUS;
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     public InitDBService() {
         super("InitDBService");
@@ -30,7 +34,11 @@ public class InitDBService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
 
+        BUS = BusProvider.getInstance();
+        BUS.register(this);
+
         AssetManager assetManager = getAssets();
+        BUS.post(new LoadingEvent(LoadingEvent.STATE_STARTED));
 
         try {
             InputStream JSONInputStream = assetManager.open("words.json");
@@ -38,6 +46,14 @@ public class InitDBService extends IntentService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        BUS.post(new LoadingEvent(LoadingEvent.STATE_FINISHED));
+    }
+
+    @Override
+    public void onDestroy() {
+        BUS.unregister(this);
+        super.onDestroy();
     }
 
     /*
