eb6f8931962d0aab6da6f424a9afd18ae56250d8
Giacomo Bresciani <brescia123@gmail.com>
2015-02-24 19:18:09 +0100

    extended Application to init db on app install

--- app/src/main/java/it/gbresciani/poligame/GiocosoApplication.java
+++ app/src/main/java/it/gbresciani/poligame/GiocosoApplication.java
@@ -1,19 +1,19 @@
 package it.gbresciani.poligame;
 
-import android.app.Application;
 import android.content.Intent;
+
+import com.orm.SugarApp;
 
 import it.gbresciani.poligame.services.InitDBService;
 
-public class GiocosoApplication extends Application {
+public class GiocosoApplication extends SugarApp {
 
     @Override
     public void onCreate() {
-        super.onCreate();
-
+        /* Start the intentService that initialize the database */
         Intent initDBIntent = new Intent(this, InitDBService.class);
         startService(initDBIntent);
+        super.onCreate();
 
-        /* Start the intentService that fill the database (check if exists) */
     }
 }
