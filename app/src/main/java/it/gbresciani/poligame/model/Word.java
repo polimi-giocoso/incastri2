39c115c6b938332e619d0df7276d080905b26d25
Giacomo Bresciani <brescia123@gmail.com>
2015-02-28 22:20:47 +0100

    removed inver_syllables

--- app/src/main/java/it/gbresciani/poligame/model/Word.java
+++ app/src/main/java/it/gbresciani/poligame/model/Word.java
@@ -8,8 +8,6 @@ public class Word extends SugarRecord<Word> {
     public final static String LEMMA = "lemma";
     public final static String SYLLABLE_1 = "syllable1";
     public final static String SYLLABLE_2 = "syllable2";
-    public final static String INVER_SYLLABLE_1 = "inverSyllable1";
-    public final static String INVER_SYLLABLE_2 = "inverSyllable3";
 
     @SerializedName(LEMMA)
     private String lemma;
@@ -20,11 +18,6 @@ public class Word extends SugarRecord<Word> {
     @SerializedName(SYLLABLE_2)
     private String syllable2;
 
-    @SerializedName(INVER_SYLLABLE_1)
-    private String inverSyllable1;
-
-    @SerializedName(INVER_SYLLABLE_2)
-    private String inverSyllable2;
 
     public Word() {
     }
@@ -33,7 +26,5 @@ public class Word extends SugarRecord<Word> {
         this.lemma = lemma;
         this.syllable1 = syllable1;
         this.syllable2 = syllable2;
-        this.inverSyllable1 = inverSyllable1;
-        this.inverSyllable2 = inverSyllable2;
     }
 }
