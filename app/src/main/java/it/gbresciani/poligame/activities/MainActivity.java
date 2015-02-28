7510789dc3540bafa88e402ff6f4ffe92a7de306
Giacomo Bresciani <brescia123@gmail.com>
2015-02-28 23:05:54 +0100

    bus + new activity intent

--- app/src/main/java/it/gbresciani/poligame/activities/MainActivity.java
+++ app/src/main/java/it/gbresciani/poligame/activities/MainActivity.java
@@ -1,21 +1,56 @@
 package it.gbresciani.poligame.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import it.gbresciani.poligame.R;
+import it.gbresciani.poligame.events.LoadingEvent;
+import it.gbresciani.poligame.helper.BusProvider;
+import it.gbresciani.poligame.services.InitDBService;
 
 
 public class MainActivity extends FragmentActivity {
+
+    private Bus BUS;
+
+    @InjectView(R.id.play_button) Button playButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BUS = BusProvider.getInstance();
+        ButterKnife.inject(this);
 
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
     }
+
+    @OnClick(R.id.play_button)
+    public void startPlayActivity(){
+        Intent playActivityIntent = new Intent(this, PlayActivity.class);
+        startActivity(playActivityIntent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        BUS.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        BUS.unregister(this);
+        super.onPause();
+    }
+
 }
