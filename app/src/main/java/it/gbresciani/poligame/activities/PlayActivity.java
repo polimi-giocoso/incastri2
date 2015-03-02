82d55a61c2304d5ab0e403e67ef39ceafd0c9ce0
Giacomo Bresciani <brescia123@gmail.com>
2015-03-02 19:17:44 +0100

    passing noSyllables to fragments

--- app/src/main/java/it/gbresciani/poligame/activities/PlayActivity.java
+++ app/src/main/java/it/gbresciani/poligame/activities/PlayActivity.java
@@ -2,7 +2,9 @@ package it.gbresciani.poligame.activities;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 
 import it.gbresciani.poligame.R;
@@ -17,29 +19,45 @@ import it.gbresciani.poligame.fragments.WordsFragment;
 * */
 public class PlayActivity extends ActionBarActivity {
 
+    private int noPages;
+    private int noSyllables;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        initUI();
+        // Get match configuration
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        noPages = sp.getInt(getString(R.string.setting_no_pages_key), 1);
+        noSyllables = sp.getInt(getString(R.string.setting_no_syllables_key), R.id.four_syllables_radio_button);
+
+        initUI(noPages, noSyllables);
     }
 
-    /*
-    * Initialize the UI, adding the two fragments
+    /**
+    * Initialize the UI, adding the two fragments.
+    *
+    * @param noPages The number of pages of the current match.
+    * @param noSyllables The number of syllables to display for the current match.
+    *
     */
-    private void initUI() {
+    private void initUI(int noPages, int noSyllables) {
 
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
 
-        // TODO: read from preferences
-        WordsFragment wordsFragment = WordsFragment.newInstance(4);
-        SyllablesFragment syllablesFragment = SyllablesFragment.newInstance(4);
+        WordsFragment wordsFragment = WordsFragment.newInstance(noSyllables);
+        SyllablesFragment syllablesFragment = SyllablesFragment.newInstance(noSyllables);
 
         ft.add(R.id.words_frame_layout, wordsFragment);
         ft.add(R.id.syllables_frame_layout, syllablesFragment);
 
         ft.commit();
 
+
+
+
+
     }
+
 }
