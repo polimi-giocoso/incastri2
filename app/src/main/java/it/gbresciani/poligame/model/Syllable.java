4a0d5df52e9cd54fd5ad2c1ea6d0f2c29b1b925e
Giacomo Bresciani <brescia123@gmail.com>
2015-03-07 17:30:32 +0100

    getter and setter + equals

--- app/src/main/java/it/gbresciani/poligame/model/Syllable.java
+++ app/src/main/java/it/gbresciani/poligame/model/Syllable.java
@@ -16,4 +16,25 @@ public class Syllable extends SugarRecord<Syllable> {
     public Syllable(String val) {
         this.val = val;
     }
+
+    public String getVal() {
+        return val;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Syllable)) return false;
+
+        Syllable syllable = (Syllable) o;
+
+        if (val != null ? !val.equals(syllable.val) : syllable.val != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return val != null ? val.hashCode() : 0;
+    }
 }
--- app/src/main/java/it/gbresciani/poligame/model/Word.java
+++ app/src/main/java/it/gbresciani/poligame/model/Word.java
@@ -27,4 +27,52 @@ public class Word extends SugarRecord<Word> {
         this.syllable1 = syllable1;
         this.syllable2 = syllable2;
     }
+
+    public String getLemma() {
+        return lemma;
+    }
+
+    public void setLemma(String lemma) {
+        this.lemma = lemma;
+    }
+
+    public String getSyllable1() {
+        return syllable1;
+    }
+
+    public void setSyllable1(String syllable1) {
+        this.syllable1 = syllable1;
+    }
+
+    public String getSyllable2() {
+        return syllable2;
+    }
+
+    public void setSyllable2(String syllable2) {
+        this.syllable2 = syllable2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Word)) return false;
+
+        Word word = (Word) o;
+
+        if (lemma != null ? !lemma.equals(word.lemma) : word.lemma != null) return false;
+        if (syllable1 != null ? !syllable1.equals(word.syllable1) : word.syllable1 != null)
+            return false;
+        if (syllable2 != null ? !syllable2.equals(word.syllable2) : word.syllable2 != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lemma != null ? lemma.hashCode() : 0;
+        result = 31 * result + (syllable1 != null ? syllable1.hashCode() : 0);
+        result = 31 * result + (syllable2 != null ? syllable2.hashCode() : 0);
+        return result;
+    }
 }
