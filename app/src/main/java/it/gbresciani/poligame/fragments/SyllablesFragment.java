035e99aab1d57d5e4847cc031b4018019b2593f1
Giacomo Bresciani <brescia123@gmail.com>
2015-03-08 18:07:16 +0100

    added select animations

--- app/src/main/java/it/gbresciani/poligame/fragments/SyllablesFragment.java
+++ app/src/main/java/it/gbresciani/poligame/fragments/SyllablesFragment.java
@@ -4,6 +4,7 @@ package it.gbresciani.poligame.fragments;
 import android.app.Fragment;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,7 +27,7 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import it.gbresciani.poligame.R;
 import it.gbresciani.poligame.activities.PlayActivity;
-import it.gbresciani.poligame.events.PageCompletedEvent;
+import it.gbresciani.poligame.customUIElements.SyllableCard;
 import it.gbresciani.poligame.events.SyllableSelectedEvent;
 import it.gbresciani.poligame.events.WordSelectedEvent;
 import it.gbresciani.poligame.helper.BusProvider;
@@ -45,7 +46,9 @@ public class SyllablesFragment extends Fragment {
     private PlayActivity mActivity;
     private Bus BUS;
     private Random rnd = new Random();
-    private ArrayList<TextView> syllableCards = new ArrayList<>();
+    private ArrayList<CardView> syllableCards = new ArrayList<>();
+    private ArrayList<Integer> syllableCardRotationDegree = new ArrayList<>();
+    private ArrayList<Boolean> syllableCardSelection = new ArrayList<>();
 
 
     @InjectView(R.id.syllables_container) LinearLayout syllablesContainerLinearLayout;
@@ -116,8 +119,8 @@ public class SyllablesFragment extends Fragment {
 
 
     @Subscribe public void wordSelected(WordSelectedEvent wordSelectedEvent) {
-        for (TextView card : syllableCards) {
-            card.setTextColor(getResources().getColor(android.R.color.black));
+        for (CardView card : syllableCards) {
+            select(card, false);
         }
     }
 
@@ -141,11 +144,11 @@ public class SyllablesFragment extends Fragment {
                 int height = syllablesContainerLinearLayout.getMeasuredHeight();
 
                 // To maintain proportions calculates the margin according to the number of slot to be displayed
-                int slotMargin = ((int) getResources().getDimension(R.dimen.slot_margin)) * 3;
+                int slotMargin = (int) (( getResources().getDimension(R.dimen.slot_margin)) * 1.5);
 
                 // Choose, as dimension for one slot, the minimum between the width of the layout and the height divided
                 // by the number of slots to be drawn minus two margins
-                int slotDimen = Math.min(width / 2, (height / (syllablesCount / 2))) - 2 * slotMargin;
+                int slotDimen = ( Math.min(width / 2, (height / (syllablesCount / 2))) - 2 * slotMargin);
 
                 // Add 2 LinearLayout
                 for (int i = 0; i < syllablesCount; i++) {
@@ -154,6 +157,7 @@ public class SyllablesFragment extends Fragment {
                     LinearLayout ll = new LinearLayout(mActivity);
                     LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                             ViewGroup.LayoutParams.MATCH_PARENT);
+                    lParams.gravity = Gravity.CENTER;
                     lParams.weight = 1;
                     ll.setLayoutParams(lParams);
 
@@ -164,8 +168,7 @@ public class SyllablesFragment extends Fragment {
                         syllablesLinearLayout2.addView(ll);
                     }
 
-                    TextView card = createSyllableCard(syllables.get(i).getVal(), slotDimen, slotMargin);
-                    syllableCards.add(card);
+                    CardView card = createSyllableCard(syllables.get(i).getVal(), slotDimen, slotMargin);
                     ll.addView(card);
 
                 }
@@ -173,18 +176,24 @@ public class SyllablesFragment extends Fragment {
         });
     }
 
-    private TextView createSyllableCard(String text, int cardDimen, int cardMargin) {
+    private CardView createSyllableCard(String text, int cardDimen, int cardMargin) {
 
-        TextView textCard = new TextView(mActivity);
+        CardView syllableCardView = new SyllableCard(mActivity);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(cardDimen, cardDimen);
         params.setMargins(cardMargin, cardMargin, cardMargin, cardMargin);
         params.gravity = Gravity.CENTER;
-        textCard.setBackgroundColor(rndColor());
-        textCard.setText(text);
-        textCard.setGravity(Gravity.CENTER);
-        textCard.setTextSize(100);
+        syllableCardView.setLayoutParams(params);
+        syllableCardView.setCardBackgroundColor(rndColor());
 
-        textCard.setLayoutParams(params);
+        TextView textView = new TextView(mActivity);
+        textView.setText(text);
+        textView.setTextSize(100);
+        textView.setTextColor(getResources().getColor(android.R.color.white));
+        textView.setGravity(Gravity.CENTER);
+        textView.setAllCaps(true);
+        textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        syllableCardView.addView(textView);
 
         // Choose a random rotation angle between -25 and 25
         final int degree = rnd.nextInt(50) - 25;
@@ -209,16 +218,71 @@ public class SyllablesFragment extends Fragment {
         animSet.addAnimation(scaleAnimation);
         animSet.addAnimation(rotateAnimation);
 
-        textCard.startAnimation(animSet);
+        syllableCardView.startAnimation(animSet);
 
-        textCard.setOnClickListener(new View.OnClickListener() {
+        syllableCardView.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-                String syllable = (String) ((TextView) v).getText();
-                ((TextView) v).setTextColor(getResources().getColor(android.R.color.white));
+                // Get the only child view that is a TextView
+                String syllable = (String) ((TextView) ((CardView) v).getChildAt(0)).getText();
+                select(v, true);
                 BUS.post(new SyllableSelectedEvent(syllable));
             }
         });
 
-        return textCard;
+        syllableCards.add(syllableCardView);
+        syllableCardRotationDegree.add(degree);
+        syllableCardSelection.add(false);
+
+        return syllableCardView;
+    }
+
+    private void select(View v, boolean selected) {
+        int cardIndex = syllableCards.indexOf(v);
+        float viewDegree = syllableCardRotationDegree.get(cardIndex);
+        boolean viewSelection = syllableCardSelection.get(cardIndex);
+        float toDegree;
+        float fromDegree;
+        float toScale;
+        float fromScale;
+        if (selected && !viewSelection) {
+            fromDegree = viewDegree;
+            toDegree = 0;
+            fromScale = 1f;
+            toScale = 1.5f;
+
+        } else if (!selected && viewSelection) {
+            fromDegree = 0;
+            toDegree = viewDegree;
+            fromScale = 1.5f;
+            toScale = 1f;
+
+        } else{
+            return;
+        }
+
+        AnimationSet animSet = new AnimationSet(true);
+        animSet.setInterpolator(new DecelerateInterpolator());
+        animSet.setFillEnabled(true);
+        animSet.setFillAfter(true);
+        animSet.setDuration(250);
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(fromScale, toScale, fromScale, toScale,
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+
+        scaleAnimation.setStartOffset(250);
+        scaleAnimation.setDuration(250);
+
+        RotateAnimation rotateAnimation = new RotateAnimation(fromDegree, toDegree,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setStartOffset(0);
+        scaleAnimation.setDuration(250);
+
+        animSet.addAnimation(scaleAnimation);
+        animSet.addAnimation(rotateAnimation);
+
+        v.startAnimation(animSet);
+        syllableCardSelection.set(cardIndex, selected);
     }
 }
