40401d90b4af64092a077b8c68456468fa0cd250
Giacomo Bresciani <brescia123@gmail.com>
2015-03-08 22:13:00 +0100

    using shared preferences to check db

--- app/src/main/java/it/gbresciani/poligame/GiocosoApplication.java
+++ app/src/main/java/it/gbresciani/poligame/GiocosoApplication.java
@@ -3,6 +3,7 @@ package it.gbresciani.poligame;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.orm.SugarApp;
 
@@ -12,10 +13,15 @@ public class GiocosoApplication extends SugarApp {
 
     @Override
     public void onCreate() {
-        // Start the intentService that initialize the database
-        Intent initDBIntent = new Intent(this, InitDBService.class);
-        startService(initDBIntent);
-
+        // Start the intentService that initialize the database if is the first run
+        Log.d("GiocosoApplication", "onCreate");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(!prefs.getBoolean("firstTime", false)) {
+            Intent initDBIntent = new Intent(this, InitDBService.class);
+            startService(initDBIntent);            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstTime", true);
+            editor.commit();
+        }
         super.onCreate();
 
     }
