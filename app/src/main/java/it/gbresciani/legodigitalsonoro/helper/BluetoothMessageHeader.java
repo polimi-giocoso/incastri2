aebf6fb5c12818a42b2d5e93737b05ccc70b4ce9
Giacomo Bresciani <brescia123@gmail.com>
2015-03-25 14:19:36 +0100

    headers for bluetooth messages

---
+++ app/src/main/java/it/gbresciani/legodigitalsonoro/helper/BluetoothMessageHeader.java
@@ -0,0 +1,12 @@
+package it.gbresciani.legodigitalsonoro.helper;
+
+/**
+ * Created by bear on 25/03/15.
+ */
+public class BluetoothMessageHeader {
+
+    public static final String PAGE_INFO = "PAGE_INFO: ";
+    public static final String SIMPLE_TURN_PASS = "SIMPLE_TURN_PASS";
+    public static final String WORD_FOUND = "WORD_FOUND: " ;
+    public static final String GAME_END = "GAME_END" ;
+}
